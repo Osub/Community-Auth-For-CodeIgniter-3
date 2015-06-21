d80fd99d82ebb7d8171d32a559f3b2b4366fa46c
Brian Gottier <iamsentme@gmail.com>
2015-06-21 16:07:18 -0700

    Making install script a little more user friendly

--- application/third_party/community_auth/sh/.install.sh
+++ application/third_party/community_auth/sh/.install.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 # core
-cp ./third_party/community_auth/core/MY_Controller.php ./core/MY_Controller.php 
-cp ./third_party/community_auth/core/MY_Input.php ./core/MY_Input.php 
-cp ./third_party/community_auth/core/MY_Model.php ./core/MY_Model.php
+if [ ! -f ./core/MY_Controller.php ]; then
+	cp ./third_party/community_auth/core/MY_Controller.php ./core/MY_Controller.php 
+fi
+
+if [ ! -f ./core/MY_Input.php ]; then
+	cp ./third_party/community_auth/core/MY_Input.php ./core/MY_Input.php 
+fi
+
+if [ ! -f ./core/MY_Model.php ]; then
+	cp ./third_party/community_auth/core/MY_Model.php ./core/MY_Model.php
+fi
+
 # hooks
-cp ./third_party/community_auth/hooks/auth_constants.php ./hooks/auth_constants.php
+if [ ! -f ./hooks/auth_constants.php ]; then
+	cp ./third_party/community_auth/hooks/auth_constants.php ./hooks/auth_constants.php
+fi
+
 # controllers
-cp ./third_party/community_auth/controllers/Examples.php ./controllers/Examples.php 
-cp ./third_party/community_auth/controllers/User.php ./controllers/User.php
+if [ ! -f ./controllers/Examples.php ]; then
+	cp ./third_party/community_auth/controllers/Examples.php ./controllers/Examples.php 
+fi
+
+if [ ! -f ./controllers/User.php ]; then
+	cp ./third_party/community_auth/controllers/User.php ./controllers/User.php
+fi
+
 # public_root
-cp ./third_party/community_auth/public_root/.htaccess ./../.htaccess
+if [ ! -f ./../.htaccess ]; 
+then
+	cp ./third_party/community_auth/public_root/.htaccess ./../.htaccess
+else
+	sed -i '1s/^/# MAKE SURE TO LEAVE THE NEXT TWO LINES HERE.\n# BEGIN DENY LIST --\n# END DENY LIST --\n\n/' ./../.htaccess
+fi
