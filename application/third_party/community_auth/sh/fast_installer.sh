36317c29159ebe49145b45ddffe6e31e0c658660
Brian Gottier <iamsentme@gmail.com>
2016-01-01 15:12:19 -0800

    Fast installer does not need to check for existing files

--- application/third_party/community_auth/sh/fast_installer.sh
+++ application/third_party/community_auth/sh/fast_installer.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+#
+# This installer allows you to quickly move through steps 1 through 7 of 
+# the installation instructions for Community Auth for CodeIgniter 3.
+# After running this script, you'll want to delete it, then proceed to step 8.
+# 
 
 # Make sure CodeIgniter doesn't already exist
 if [ ! -d ./application ]; 
 then
+	# Temp file deleted after extracts
 	TMPFILE=`mktemp`
 
 	# Download and extract CodeIgniter
@@ -19,46 +25,24 @@ then
 	cd ../
 
 	# Copy core files from Community Auth to CodeIgniter application
-	if [ ! -f ./core/MY_Controller.php ]; then
-		cp ./third_party/community_auth/core/MY_Controller.php ./core/MY_Controller.php 
-	fi
-
-	if [ ! -f ./core/MY_Input.php ]; then
-		cp ./third_party/community_auth/core/MY_Input.php ./core/MY_Input.php 
-	fi
-
-	if [ ! -f ./core/MY_Model.php ]; then
-		cp ./third_party/community_auth/core/MY_Model.php ./core/MY_Model.php
-	fi
+	cp ./third_party/community_auth/core/MY_Controller.php ./core/MY_Controller.php
+	cp ./third_party/community_auth/core/MY_Input.php ./core/MY_Input.php 
+	cp ./third_party/community_auth/core/MY_Model.php ./core/MY_Model.php
 
 	# Copy hook files from Community Auth to CodeIgniter application
-	if [ ! -f ./hooks/auth_constants.php ]; then
-		cp ./third_party/community_auth/hooks/auth_constants.php ./hooks/auth_constants.php
-	fi
-
-	if [ ! -f ./hooks/auth_sess_check.php ]; then
-		cp ./third_party/community_auth/hooks/auth_sess_check.php ./hooks/auth_sess_check.php
-	fi
+	cp ./third_party/community_auth/hooks/auth_constants.php ./hooks/auth_constants.php
+	cp ./third_party/community_auth/hooks/auth_sess_check.php ./hooks/auth_sess_check.php
 
 	# Copy controller files from Community Auth to CodeIgniter application
-	if [ ! -f ./controllers/Examples.php ]; then
-		cp ./third_party/community_auth/controllers/Examples.php ./controllers/Examples.php 
-	fi
-
-	if [ ! -f ./controllers/Key_creator.php ]; then
-		cp ./third_party/community_auth/controllers/Key_creator.php ./controllers/Key_creator.php 
-	fi
+	cp ./third_party/community_auth/controllers/Examples.php ./controllers/Examples.php 
+	cp ./third_party/community_auth/controllers/Key_creator.php ./controllers/Key_creator.php 
 
 	# Copy or modify main .htaccess
-	if [ ! -f ./../.htaccess ]; 
-	then
-		cp ./third_party/community_auth/public_root/.htaccess ./../.htaccess
-	else
-		sed -i '1s/^/# MAKE SURE TO LEAVE THE NEXT TWO LINES HERE.\n# BEGIN DENY LIST --\n# END DENY LIST --\n\n/' ./../.htaccess
-	fi
+	cp ./third_party/community_auth/public_root/.htaccess ./../.htaccess
 
+	# Success
 	echo "REMOVE INSTALLER SCRIPT (THIS FILE) AND PROCEED TO INSTALLATION STEP 8."
-
 else
+	# Error
 	echo "INSTALLER ERROR: APPLICATION ALREADY INSTALLED."
 fi
