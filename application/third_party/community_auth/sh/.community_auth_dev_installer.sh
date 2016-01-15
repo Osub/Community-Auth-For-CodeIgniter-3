20cc1eca139f34a368b3a6ea71d99e705f03a563
Brian Gottier <iamsentme@gmail.com>
2016-01-14 21:01:50 -0800

    Variables make the dev installer more awesome

--- application/third_party/community_auth/sh/.community_auth_dev_installer.sh
+++ application/third_party/community_auth/sh/.community_auth_dev_installer.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-# This is the installer for those working on Community Auth development
+# This is the installer for those working on Community Auth development.
+# Place and execute this script where IT WILL CREATE the web root directory.
+# It will also create the database, as long as you set a valid user and password.
+
+SITEDIR="community_auth_ci_3"
+BASEURL="http:\/\/localhost.community_auth_ci_3\/"
+DBNAME="community_auth_ci_3"
+DBUSER="root"
+DBPASS=""
 
 # Make sure not already installed
-if [ ! -d ./community_auth_ci_3 ]; 
+if [ ! -d ./$SITEDIR ]; 
 then
 	# Clone the Community Auth repository on bitbucket
-	git clone git@bitbucket.org:skunkbad/community-auth-for-codeigniter-3.git community_auth_ci_3
+	git clone git@bitbucket.org:skunkbad/community-auth-for-codeigniter-3.git $SITEDIR
 
 	# The web root directory should now exist
-	if [ -d ./community_auth_ci_3 ];
+	if [ -d ./$SITEDIR ];
 	then
 		# Move into web root
-		cd ./community_auth_ci_3
+		cd ./$SITEDIR
 
 		# Temporarily move Community Auth
 		mv ./application/third_party/community_auth/ ./community_auth_tmp/
@@ -58,7 +66,7 @@ s/\['model'\] = array()/\['model'\] = array(\n\t'auth_model'\n)/g;" ./config/aut
 		echo "\$route[LOGIN_PAGE] = 'examples/login';" >> ./config/routes.php
 
 		# Set base_url, index_page, turn hooks on, add an encryption key, and configure sessions
-		sed -i "s/\['base_url'\] = ''/\['base_url'\] = 'http:\/\/localhost.community_auth_ci_3\/'/g; \
+		sed -i "s/\['base_url'\] = ''/\['base_url'\] = '$BASEURL'/g; \
 s/\['index_page'\] = 'index.php'/\['index_page'\] = ''/g; \
 s/\['enable_hooks'\] = FALSE/\['enable_hooks'\] = TRUE/g; \
 s/\['encryption_key'\] = ''/\['encryption_key'\] = hex2bin('5d3a06b1a1efeb861ad761fb8839794f')/g; \
@@ -71,13 +79,25 @@ s/\['sess_regenerate_destroy'\] = FALSE/\['sess_regenerate_destroy'\] = TRUE/g;"
 		printf "\$hook['pre_system'] = array(\n\t'function' => 'auth_constants',\n\t'filename' => 'auth_constants.php',\n\t'filepath' => 'hooks'\n);\n\$hook['post_system'] = array(\n\t'function' => 'auth_sess_check',\n\t'filename' => 'auth_sess_check.php',\n\t'filepath' => 'hooks'\n);" >> ./config/hooks.php
 
 		# Create DB
-		DBNAME=community_auth_ci_3
-		mysqladmin -u root create $DBNAME
-		cat ./third_party/community_auth/sql/install.sql | mysql -u root $DBNAME
+		if [ -n "$DBPASS" ];
+		then
+			mysqladmin -u $DBUSER -p$DBPASS create $DBNAME
+			cat ./third_party/community_auth/sql/install.sql | mysql -u $DBUSER -p$DBPASS $DBNAME
+		else
+			mysqladmin -u $DBUSER create $DBNAME
+			cat ./third_party/community_auth/sql/install.sql | mysql -u $DBUSER $DBNAME
+		fi
 
 		# Configure DB
-		sed -i "s/'username' => ''/'username' => 'root'/g; \
-s/'database' => ''/'database' => 'community_auth_ci_3'/g;" ./config/database.php
+		if [ -n "$DBPASS" ];
+		then
+			sed -i "s/'username' => ''/'username' => '$DBUSER'/g; \
+s/'password' => ''/'password' => '$DBPASS'/g; \
+s/'database' => ''/'database' => '$DBNAME'/g;" ./config/database.php
+		else
+			sed -i "s/'username' => ''/'username' => '$DBUSER'/g; \
+s/'database' => ''/'database' => '$DBNAME'/g;" ./config/database.php
+		fi
 
 		# Pretty URLs
 		printf "\n\nRewriteEngine On\nRewriteBase /\n\nRewriteRule ^(system|application|cgi-bin) - [F,L]\nRewriteCond %%{REQUEST_FILENAME} !-f\nRewriteCond %%{REQUEST_FILENAME} !-d\nRewriteRule .* index.php/\$0 [PT,L]" >> ./../.htaccess
