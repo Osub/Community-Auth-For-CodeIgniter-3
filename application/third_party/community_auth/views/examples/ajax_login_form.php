c69afb059a86ee1bd5973343d3085210d0719db5
Brian Gottier <iamsentme@gmail.com>
2016-01-23 22:40:55 -0800

    File belongs with last commit

---
+++ application/third_party/community_auth/views/examples/ajax_login_form.php
@@ -0,0 +1,60 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Community Auth - Ajax Login Form View
+ *
+ * Community Auth is an open source authentication application for CodeIgniter 3
+ *
+ * @package     Community Auth
+ * @author      Robert B Gottier
+ * @copyright   Copyright (c) 2011 - 2016, Robert B Gottier. (http://brianswebdesign.com/)
+ * @license     BSD - http://www.opensource.org/licenses/BSD-3-Clause
+ * @link        http://community-auth.com
+ */
+
+?>
+
+<h1>Ajax login</h1>
+<p>Open up your javascript console to see what is happening when you try to login</p>
+
+<?php
+
+echo form_open( 'examples/ajax_attempt_login', array( 'class' => 'std-form' ) );
+
+?>
+
+	<div>
+
+		<label for="login_string" class="form_label">Username or Email</label>
+		<input type="text" name="login_string" id="login_string" class="form_input" autocomplete="off" maxlength="255" />
+
+		<br />
+
+		<label for="login_pass" class="form_label">Password</label>
+		<input type="password" name="login_pass" id="login_pass" class="form_input password" maxlength="<?php echo config_item('max_chars_for_password'); ?>" autocomplete="off" readonly="readonly" onfocus="this.removeAttribute('readonly');" />
+
+
+		<?php
+			if( config_item('allow_remember_me') )
+			{
+		?>
+
+			<br />
+
+			<label for="remember_me" class="form_label">Remember Me</label>
+			<input type="checkbox" id="remember_me" name="remember_me" value="yes" />
+
+		<?php
+			}
+		?>
+
+		<input type="submit" name="submit" value="Login" id="submit_button"  />
+
+	</div>
+</form>
+
+<?php
+
+/* End of file login_form.php */
+/* Location: /views/examples/login_form.php */ 
